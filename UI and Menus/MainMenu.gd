cd4d003c219ecbfbe9ce0dd2a236b6298d769216
ajayamoorthy <96484574+ajayamoorthy@users.noreply.github.com>
2023-11-14 22:55:52 -0500

    Ugly Main Menu with Basic Functionality Implemented
    the main menu is ugly but it is there, start and quit buttons are functional (start -> maze, quit actually quits)

---
+++ UI and Menus/MainMenu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://Scenes/MazeMinigameLevel1.tscn")
+	
+func _on_Saves_pressed():
+	#get_tree().change_scene("res://Scenes/ [saves scene] ")
+	pass # Replace with function body.
+
+func _on_SettingsButton_pressed():
+	#var settings = load(scene for settings).instance()
+	#get_tree().current_scene.add_child(settings)
+	pass
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+
+
