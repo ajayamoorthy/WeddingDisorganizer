5434f96b8a13f9a0d9d5c47e7c84f236b7669aca
ajayamoorthy <96484574+ajayamoorthy@users.noreply.github.com>
2023-12-07 12:53:58 -0500

    Added dialogue to office room scene
    dialogue in the office scene should work

--- Scenes/OfficeRoom.gd
+++ Scenes/OfficeRoom.gd
@@ -4,28 +4,29 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var budget;
+var localBudget;
 
-var broom1satisfaction = 0;
-var broom2satisfaction = 0;
+#var broom1satisfaction = 0;
+#var broom2satisfaction = 0;
 
-var minigames = ["MazeMinigameLevel1", "RhythmMinigame", "DressSelectionMinigame", "DriveToBakeriesMinigame", "SeatingArrangementMinigame", "BudgetingMinigame", "PlanningMinigame", "OfficeRoom"]; #which scenes
-var minigameOrder = [-1, -1, -1, -1, -1];
-var whichBroomDoesWhat = [-1, -1, -1, -1, -1]; #0 = both, 1 = nic, 2 = chauntal
-var minigameComplete = [0, 0, 0, 0, 0];
+#var minigames = ["MazeMinigameLevel1", "RhythmMinigame", "DressSelectionMinigame", "DriveToBakeriesMinigame", "SeatingArrangementMinigame", "BudgetingMinigame", "PlanningMinigame", "OfficeRoom"]; #which scenes
+#var minigameOrder = [-1, -1, -1, -1, -1];
+#var whichBroomDoesWhat = [-1, -1, -1, -1, -1]; #0 = both, 1 = nic, 2 = chauntal
+#var minigameComplete = [0, 0, 0, 0, 0];
 
-var colorPalette;
-var broom1Fit;
-var broom2Fit;
-var floristSelection;
-var bakerSelection;
-var seatingHarmony;
+#var colorPalette;
+#var broom1Fit;
+#var broom2Fit;
+#var floristSelection;
+#var bakerSelection;
+#var seatingHarmony;
 
-var current_scene = null;
+#var current_scene = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var resource = preload("res://Dialogue/nicAndChauntalDialogue.tres")
+	DialogueManager.show_example_dialogue_balloon("start_node", resource)
 
 func _on_ContractConfirmed_pressed():
 	Global.goto_scene("res://Scenes/BudgetingMinigame.tscn")
---
+++ manageDialogue.gd
@@ -0,0 +1,17 @@
+extends Node
+
+#var resource = preload("res://Dialogue/nicAndChauntalDialogue.tres")
+
+func show_dialogue(title: String, resource: DialogueResource = null) -> void:
+	#DialogueManager.show_example_dialogue_balloon(resource)
+	var dialogue = yield(DialogueManager.get_next_dialogue_line(title, resource), "completed")  
+	
+	if dialogue != null:
+		var balloon = preload("res://addons/dialogue_manager/example_balloon/example_balloon.tscn").instance()
+		balloon.dialogue = dialogue
+		get_tree().current_scene.add_child(balloon)
+		show_dialogue(yield(balloon, "actioned"), resource)
+		
+	  
+	
+	
