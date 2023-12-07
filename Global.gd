21e737cae9f54f1d67beaa17eef75faab5e7a5e5
ajayamoorthy <96484574+ajayamoorthy@users.noreply.github.com>
2023-12-06 20:24:06 -0500

    global scene switching function created
    function implemented, not tested

--- Global.gd
+++ Global.gd
@@ -1,16 +1,44 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var budget;
 
+var broom1satisfaction = 0;
+var broom2satisfaction = 0;
+
+var minigames = ["res://Scenes/MazeMinigameLevel1.tscn", "res://Scenes/RhythmMinigame.tscn", "res://Scenes/DressSelectionMinigame.tscn", "res://Scenes/DriveToBakeriesMinigame.tscn", "res://Scenes/SeatingArrangementMinigame.tscn", "res://Scenes/BudgetingMinigame.tscn", "res://Scenes/PlanningMinigame.tscn", "res://Scenes/OfficeRoom.tscn"] #which scenes
+var minigameOrder = ["", "", "", "", ""]
+var whichBroomDoesWhat = [-1, -1, -1, -1, -1] #0 = both, 1 = nic, 2 = chauntal
+var minigameComplete = [0, 0, 0, 0, 0]
+
+
+var currentScene = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var root = get_tree().root
+	currentScene = root.get_child(root.get_child_count() - 1)
+	
+func goto_scene(path):
+	call_deferred("_deferred_goto_scene", path)
+	
+func _deferred_goto_scene(path):
+	#remove current scene
+	currentScene.free()
+	
+	#load the new scene
+	var newScene = ResourceLoader.load(path)
+	
+	#instance the new scene
+	currentScene = newScene.instance()
+	
+	#add it to the active scene (child of root)
+	get_tree().root.add_child(currentScene)
+	
+func updateSatisfaction(broom, amount):
+	if broom == "nic":
+		broom1satisfaction += amount
+	elif broom == "chauntal":
+		broom2satisfaction += amount
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
